50d26dacf04ece7fcb1788af074028556f9dd154
Ganesan <brf4@M-C02S80T6G8WM.nordstrom.net>
2017-06-16 11:07:06 -0700

    Test Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.Auth;
+using Amazon.S3;
+using Amazon.S3.Model;
+namespace VS_Code_Test
+{class UploadObject
+    {
+        static string bucketName = "*** bucket name ***";
+        static string keyName    = "*** key name when object is created ***";
+        static string filePath   = "*** absolute path to a sample file to upload ***";
+
+        static IAmazonS3 client;
+
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine("Test2");
+            Console.WriteLine("Test3");
+            Console.WriteLine("Test4");
+            Console.WriteLine("Test5");
+            Console.WriteLine("Test6");
+            Console.WriteLine("Test7");
+
+        }
+    }
+}
